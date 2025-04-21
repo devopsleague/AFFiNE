4605d197727a2886d401e5b5fa167455b3709b9e
flrande <flrande.ch@gmail.com>
2025-04-21 14:20:40 +0800

    fix: fix

--- blocksuite/affine/blocks/code/src/preview/renderer/html.ts
+++ blocksuite/affine/blocks/code/src/preview/renderer/html.ts
@@ -2,25 +2,31 @@ import { html } from 'lit';
 
 import { CodeBlockPreviewExtension } from '../code-preview-extension';
 
-// listen event from iframe
-window.addEventListener('message', (evt: MessageEvent) => {
-  if (evt.data?.type === 'code-block-html-iframe-height') {
-    const blockId = evt.data.blockId;
-    document
-      .querySelectorAll(`[data-block-id="${blockId}"] iframe`)
-      .forEach(iframe => {
-        if ((iframe as HTMLIFrameElement).contentWindow === evt.source) {
-          iframe.setAttribute(
-            'style',
-            `width:100%; border:none; height:${evt.data.height}px`
-          );
-        }
-      });
-  }
-});
-
 export const CodeBlockHtmlPreview = CodeBlockPreviewExtension('html', model => {
   if (model.props.language !== 'html') return null;
+
+  const abortController = new AbortController();
+  window.addEventListener(
+    'message',
+    (evt: MessageEvent) => {
+      if (evt.data?.type === 'code-block-html-iframe-height') {
+        document
+          .querySelectorAll(`[data-block-id="${model.id}"] iframe`)
+          .forEach(iframe => {
+            if ((iframe as HTMLIFrameElement).contentWindow === evt.source) {
+              iframe.setAttribute(
+                'style',
+                `width:100%; border:none; height:${evt.data.height}px`
+              );
+            }
+          });
+      }
+    },
+    { signal: abortController.signal }
+  );
+  model.deleted.subscribe(() => {
+    abortController.abort();
+  });
 
   const htmlSrc = `
     <style>
@@ -31,7 +37,7 @@ export const CodeBlockHtmlPreview = CodeBlockPreviewExtension('html', model => {
       // calculate and report height
       function reportHeight() {
         const h = document.documentElement.scrollHeight;
-        parent.postMessage({ type: 'code-block-html-iframe-height', height: h, blockId: ${JSON.stringify(model.id)} }, '*');
+        parent.postMessage({ type: 'code-block-html-iframe-height', height: h }, '*');
       }
       window.addEventListener('load', reportHeight);
       // listen content changes
